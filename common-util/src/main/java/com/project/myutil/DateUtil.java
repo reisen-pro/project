59c65bfe657c81b4a1dcb626d0129e2582736516
reisen-pro <1054004534@qq.com>
2020-08-28 01:39:59 +0800

    使用反射，调用具体类自己的now方法进行format。 值得思考的是，这样是否有意义。

--- common-util/src/main/java/com/project/myutil/DateUtil.java
+++ common-util/src/main/java/com/project/myutil/DateUtil.java
@@ -1,5 +1,6 @@
 package com.project.myutil;
 
+import java.lang.reflect.InvocationTargetException;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.Temporal;
@@ -11,77 +12,78 @@ import java.util.Date;
 public class DateUtil {
 
     /**
-     * get now time
-     *
-     * @return time String
-     * @apiNote 返回当前时间 时分秒格式字符串
-     */
-    public static String getCurrentTime(String format) {
-        return afterFormat(LocalDateTime.now(), format);
-    }
-
-    /**
-     *
-     */
-    public static String getCurrentTime() {
-        return getCurrentTime("HH:mm:ss");
-    }
-
-
-    /**
-     * 获取当前时间
-     *
-     * @param format 根据日期格式返回字符串
-     * @return 年月日
-     * @apiNote
-     */
-    public static String getCurrentDate(String format) {
-        return afterFormat(LocalDate.now(), format);
-    }
-
-    /**
      * 获取当前时间
      *
      * @return Date 无时分秒
      */
     public static Date getCurrentDate() {
         LocalDate localDate = LocalDate.now();
-        return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        return localDateToDate(localDate);
     }
+
+    /**
+     * 返回格式化以后的字符串
+     *
+     * @return String
+     */
+    public static String getCurrentDateTimeStr(String format) {
+        String str = null;
+        try {
+            str = afterFormat(LocalDateTime.class, format);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    /**
+     * 返回日期的字符串
+     *
+     * @return String
+     */
+    public static String getCurrentDateStr() {
+        return getCurrentDateTimeStr("yyyy-MM-dd");
+    }
+
+    /**
+     * 返回HH:mm:ss格式的时间
+     *
+     * @return String
+     */
+    public static String getCurrentTimeStr() {
+        return getCurrentDateTimeStr("HH:mm:ss");
+    }
+
+    /**
+     * 返回年-月-日 时：分：秒格式的字符串
+     *
+     * @return String
+     */
+    public static String getCurrentDateTimeStr() {
+        return getCurrentDateTimeStr("yyyy-MM-dd HH:mm:ss");
+    }
+
 
     /**
      * 返回当天加上一定天数以后的日期
      *
-     * @param addDays 增加天数
+     * @param plus 增加天数
      * @return 增加后的天数
      */
     public static Date plusDays(int plus) {
         LocalDate localDate = LocalDate.now().plusDays(plus);
-        return LocalDateToDate(localDate);
+        return localDateToDate(localDate);
     }
 
     /**
      * 返回当天减去一定天数以后的日期
      *
-     * @param minusDay 减去天数
+     * @param minus 减去天数
      * @return 减去后的天数
      */
     public static Date minusDays(int minus) {
         LocalDate localDate = LocalDate.now().minusDays(minus);
-        return LocalDateToDate(localDate);
-    }
-
-    /**
-     * 加上指定分钟后的时间戳
-     *
-     * @param addMinutes 需要加的分钟数
-     * @return 时间戳
-     */
-    public static long afterPlus(LocalDateTime localDateTime, int addMinutes) {
-        localDateTime = localDateTime.plusMinutes(addMinutes);
-        Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
-        long l = instant.toEpochMilli();
-        return l;
+        return localDateToDate(localDate);
     }
 
     /**
@@ -92,36 +94,62 @@ public class DateUtil {
      * @return localDateTime
      */
     public static LocalDateTime formatLocalDateTime(Date date, String time) {
-        LocalDate localDate = Instant.ofEpochMilli(date.getTime())
-                .atZone(ZoneId.systemDefault())
-                .toLocalDate();
+        LocalDate localDate = dateToLocalDate(date);
         LocalTime localTime = LocalTime.parse(time);
         return LocalDateTime.of(localDate, localTime);
     }
 
-    public static Date LocalDateToDate(LocalDate localDate) {
+    /**
+     * LocalDate 转 Date
+     *
+     * @param localDate param
+     * @return Date
+     */
+    public static Date localDateToDate(LocalDate localDate) {
         return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
     }
 
     /**
-     * 根据类型匹配，返回格式化以后的字符串，此写法不够优雅。暂时水平有限，希望如果有人能看到，看看有没有更好的写法
+     * Date 转 LocalDate
+     *
+     * @param date date
+     * @return localDate
+     */
+    public static LocalDate dateToLocalDate(Date date) {
+        return Instant.ofEpochMilli(date.getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
+
+    /**
+     * 返回格式化以后的字符串。
      *
      * @param temporal 时间类型
      * @param format   格式化
      * @return String
      */
     public static String afterFormat(Temporal temporal, String format) {
-        String afterFormat = null;
         if (temporal instanceof LocalDate) {
-            afterFormat = LocalDate.now().format(DateTimeFormatter.ofPattern(format));
+            return LocalDate.now().format(DateTimeFormatter.ofPattern(format));
         }
         if (temporal instanceof LocalTime) {
-            afterFormat = LocalTime.now().format(DateTimeFormatter.ofPattern(format));
+            return LocalTime.now().format(DateTimeFormatter.ofPattern(format));
         }
         if (temporal instanceof LocalDateTime) {
-            afterFormat = LocalDateTime.now().format(DateTimeFormatter.ofPattern(format));
+            return LocalDateTime.now().format(DateTimeFormatter.ofPattern(format));
         }
-        return afterFormat;
+        return null;
     }
 
+    /**
+     * 使用反射，返回格式化后的日期字符串
+     *
+     * @param c      class
+     * @param format format
+     * @return String
+     */
+    public static String afterFormat(Class<?> c, String format) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Object o = c.getMethod("now").invoke(null);
+        return (String) o.getClass().getMethod("format", DateTimeFormatter.class).invoke(o, DateTimeFormatter.ofPattern(format));
+    }
 }
