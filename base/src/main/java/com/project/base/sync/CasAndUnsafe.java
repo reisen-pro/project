b9660ebc18122b4c0b4a4fb19ae518753923b73b
reisen-pro <1054004534@qq.com>
2020-11-04 00:15:48 +0800

    CasAndUnsafe

---
+++ base/src/main/java/com/project/base/sync/CasAndUnsafe.java
@@ -0,0 +1,41 @@
+package com.project.base.sync;
+
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author Reisen
+ * @title: CasAndUnsafe
+ * @projectName project
+ * @description: TODO
+ * @date 2020/11/3 23:11
+ */
+public class CasAndUnsafe {
+    // 定义一个全局变量m
+    private static int m = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        // 定义一个线程数组 大小100
+        Thread[] threads = new Thread[100];
+        // countDownLatch这个类使一个线程等待其他线程各自执行完毕后再执行。
+        CountDownLatch latch = new CountDownLatch(threads.length);
+
+        // 循环100次
+        for (int i = 0; i < threads.length; i++) {
+            // 一个m+10000次的线程
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < 10000; j++) {
+                    m++;
+                }
+                latch.countDown();
+            });
+        }
+
+        // 线程启动
+        Arrays.stream(threads).forEach((t) -> t.start());
+
+        latch.await();
+
+        System.out.println(m);
+    }
+}
