2092564c085c2e222379480b809fc987e9fe2069
reisen-pro <1054004534@qq.com>
2020-11-23 17:37:24 +0800

    spring mvc action案例 配置视图解析器 页面跳转配置

--- spring/spring-mvc/src/main/java/com/project/spring/mvc/action/BytAction.java
+++ spring/spring-mvc/src/main/java/com/project/spring/mvc/action/BytAction.java
@@ -1,15 +1,15 @@
 package com.project.spring.mvc.action;
 
-
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class HelloAction implements Controller {
+public class BytAction implements Controller {
     @Override
     public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
 
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("hello");
+        // 未配置视图解析器  modelAndView.setViewName("/bye.jsp");
+        modelAndView.setViewName("bye");
         return modelAndView;
     }
 }
---
+++ spring/spring-mvc/src/main/java/com/project/spring/mvc/action/OkAction.java
@@ -0,0 +1,6 @@
+package com.project.spring.mvc.action;
+
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
+
+public class OkAction extends ParameterizableViewController {
+}
