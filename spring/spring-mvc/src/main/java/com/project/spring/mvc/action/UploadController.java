e72de35f1f3f6244dc9220611cc69256bb6e2038
reisen-pro <1054004534@qq.com>
2020-11-27 18:40:59 +0800

    spring mvc 文件上传

--- spring/spring-mvc/src/main/java/com/project/spring/mvc/action/UploadController.java
+++ spring/spring-mvc/src/main/java/com/project/spring/mvc/action/UploadController.java
@@ -1,14 +1,25 @@
 package com.project.spring.mvc.action;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-public class UploadController {
+public class UploadController implements BeanFactoryAware {
+    private BeanFactory beanFactory;
+
     @RequestMapping(value = "/upload.action",method = RequestMethod.POST)
     public void upload(MultipartFile file) {
+        beanFactory.containsBean("uploadController");
         System.out.println(file.getOriginalFilename());
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
     }
 }
