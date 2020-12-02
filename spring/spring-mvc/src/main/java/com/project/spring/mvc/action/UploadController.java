998d9415296eaf111e8429d92124648abb9a9942
reisen-pro <1054004534@qq.com>
2020-12-02 18:56:59 +0800

    校验分组

--- spring/spring-mvc/src/main/java/com/project/spring/mvc/action/UploadController.java
+++ spring/spring-mvc/src/main/java/com/project/spring/mvc/action/UploadController.java
@@ -1,6 +1,7 @@
 package com.project.spring.mvc.action;
 
 import com.project.spring.mvc.entity.Items;
+import com.project.spring.mvc.group.ValidGroup1;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
@@ -19,14 +20,15 @@ import java.util.List;
 public class UploadController implements BeanFactoryAware {
     private BeanFactory beanFactory;
 
-    @RequestMapping(value = "/upload.action",method = RequestMethod.POST)
+    @RequestMapping(value = "/upload.action", method = RequestMethod.POST)
     public void upload(MultipartFile file) {
         beanFactory.containsBean("uploadController");
         System.out.println("file.getOriginalFilename()");
     }
 
+    // 可以在Validated注解下指定使用哪个组
     @PostMapping("/validation.action")
-    public void validation(@Validated Items items, BindingResult bindingResult) {
+    public void validation(@Validated(value = {ValidGroup1.class}) Items items, BindingResult bindingResult) {
         List<ObjectError> allErrors = bindingResult.getAllErrors();
         for (ObjectError objectError : allErrors) {
             System.out.println(objectError.getDefaultMessage());
