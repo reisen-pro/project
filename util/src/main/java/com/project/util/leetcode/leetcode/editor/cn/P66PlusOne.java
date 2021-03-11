1b6fd41bbf84e35547a85aed519dc89a137997ff
reisen-pro <1054004534@qq.com>
2021-03-11 19:28:09 +0800

    leetcode 66题 +1

--- util/src/main/java/com/project/util/leetcode/leetcode/editor/cn/P66PlusOne.java
+++ util/src/main/java/com/project/util/leetcode/leetcode/editor/cn/P66PlusOne.java
@@ -42,18 +42,54 @@
 
 
 package com.project.util.leetcode.leetcode.editor.cn;
+
+import java.util.Arrays;
+
 //Java：加一
-public class P66PlusOne{
+public class P66PlusOne {
     public static void main(String[] args) {
         Solution solution = new P66PlusOne().new Solution();
+        System.out.println(Arrays.toString(solution.plusOne(new int[]{8, 9, 9})));
         // TO TEST
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int[] plusOne(int[] digits) {
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] plusOne(int[] digits) {
+            boolean flag = true;
+            for (int digit : digits) {
+                if (digit != 9) flag = false;
+            }
+            // 全是9
+            if (flag) {
+                int[] result = new int[digits.length + 1];
+                for (int i = 1; i < result.length; i++) {
+                    result[i] = 0;
+                }
+                result[0] = 1;
+                return result;
+            }else {
+                int len = digits.length - 1;
+                int count = 0;
+                if (digits[len] == 9) {
+                    for (int j = len; j > 0; j--) {
+                        if (digits[j] == 9) {
+                            count ++;
+                        }else {
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < count ; i++) {
+                        digits[len - i] = 0;
+                    }
+                    digits[len-count] = digits[len-count] + 1;
+                }else {
+                    digits[len] = digits[len] + 1;
+                }
+            }
+            return digits;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
