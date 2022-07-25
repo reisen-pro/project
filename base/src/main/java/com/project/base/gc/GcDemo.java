6106e0b83eeb84317e66fab0f8d13e053b1b7244
reisen-pro <1054004534@qq.com>
2022-07-26 00:20:24 +0800

    标志符、关键字 分布式ID课程听后记录的笔记

---
+++ base/src/main/java/com/project/base/gc/GcDemo.java
@@ -0,0 +1,11 @@
+package com.project.base.gc;
+
+/**
+ * java消除了程序员回收无用内存的职责；
+ * GC的自动回收，提高了内存空间的利用效率，很大程度上减轻了因为没有释放空间而导致的内存泄漏。
+ */
+public class GcDemo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ base/src/main/java/com/project/base/keyboard/Keyboard.java
@@ -0,0 +1,7 @@
+package com.project.base.keyboard;
+
+/**
+ * https://baike.baidu.com/item/java%E5%85%B3%E9%94%AE%E5%AD%97/5808816?fr=aladdin
+ */
+public class Keyboard {
+}
